97b843e628caceab5ec83bd919cefe424dc0c15a
Tristan <Tristanml_88@hotmail.com>
2023-11-10 00:48:32 -0500

    8 Modifier fichier sur la branche main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import click
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
+
+@click.command("guten-tag")
+def bonjour():
+    click.echo("guten tag")
 
 @click.command("version")
 def version():
